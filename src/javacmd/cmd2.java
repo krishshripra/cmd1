e44a0a56ba1ea8f124f62d2582c1b48f5290e186
shrikrishna dound <shrikrishna.dound@gmail.com>
2023-06-22 10:22:37 +0530

    cmd1

---
+++ src/javacmd/cmd2.java
@@ -0,0 +1,15 @@
+package javacmd;
+
+public class cmd2 {
+
+	public static void main(String[] args) {
+		String s="krishna";
+		if(s.contains("krish")) {
+			System.out.println("yes");
+		}else {
+			System.out.println("no");
+		}
+
+	}
+
+}
